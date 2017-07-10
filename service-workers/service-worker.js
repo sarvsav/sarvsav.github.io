1dc13c1eda9f64a8c7004d97234e74bb98142ba9
sarvsav <sarvsav@gmail.com>
2017-07-10 07:36:01 +0530

    adding offline page

--- service-workers/service-worker.js
+++ service-workers/service-worker.js
@@ -1,36 +1,55 @@
+'use strict';
+
 var version = 1;
-var cacheName = 'stale- ' + version;
+var currentCache = {
+  offline: 'offline-cache' + version
+};
+
+var offlineUrl = 'offline.html';
 
 self.addEventListener('install', function(event) {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-    if (self.clients && clients.claim) {
-        clients.claim();
-    }
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+        offlineUrl
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-  
+  var request = event.request,
+    isRequestMethodGET = request.method === 'GET';
+
+  if (request.mode === 'navigate' || isRequestMethodGET) {
     event.respondWith(
-        fetch(event.request).then(function(response) {
-            caches.open(cacheName).then(function(cache) {
-                
-                if(response.status >= 500) {
-                    cache.match(event.request).
-					then(function(response) {
-                        
-                        return response;
-                    }).catch(function() {
-                     
-                        return response;
-                    });
-                } else {
-                      cache.put(event.request, response.clone());
-                    return response;
-                }
-            });
-        })
+      fetch(createRequestWithCacheBusting(request.url)).catch(function(error) {
+        console.log('OFFLINE: Returning offline page.', error);
+        return caches.match(offlineUrl);
+      })
     );
+  } else {
+    event.respondWith(caches.match(request)
+        .then(function (response) {
+        return response || fetch(request);
+      })
+    );
+  }
 });
+function createRequestWithCacheBusting(url) {
+  var request,
+    cacheBustingUrl;
+
+  request = new Request(url,
+    {cache: 'reload'}
+  );
+
+  if ('cache' in request) {
+    return request;
+  }
+
+  cacheBustingUrl = new URL(url, self.location.href);
+  cacheBustingUrl.search += (cacheBustingUrl.search ? '&' : '') + 'cachebust=' + Date.now();
+
+  return new Request(cacheBustingUrl);
+}
